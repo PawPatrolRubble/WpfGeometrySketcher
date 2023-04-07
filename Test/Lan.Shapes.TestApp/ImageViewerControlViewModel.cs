2e40ca03ede99f95def361c89135f64e666d2edf
liwansheng <liwansheng@megarobo.tech>
2023-04-07 20:19:30 +0800

    增加zoomin和zoomOut功能，可以通过更改scale值实现不同倍率的缩放。

--- Test/Lan.Shapes.TestApp/ImageViewerControlViewModel.cs
+++ Test/Lan.Shapes.TestApp/ImageViewerControlViewModel.cs
@@ -11,11 +11,13 @@ using Lan.Shapes.Shapes;
 using Lan.Shapes.Styler;
 using Lan.SketchBoard;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 
 namespace Lan.Shapes.App
 {
     public class ImageViewerControlViewModel : ObservableObject, IImageViewerViewModel
     {
+        private const double _scaleIncremental = 0.1;
 
         private IShapeStyler _shapeStyler;
 
@@ -55,8 +57,28 @@ namespace Lan.Shapes.App
         /// </summary>
         public ImageSource Image { get; set; }
 
+
+        private double _scale;
+
+        public double Scale
+        {
+            get => _scale;
+            set
+            {
+                SetProperty(ref _scale, value);
+            }
+        }
+
+        public ICommand ZoomOutCommand { get; set; }
+        public ICommand ZoomInCommand { get; set; }
+        public ICommand ScaleToOriginalSizeCommand { get; set; }
+        public ICommand ScaleToFitCommand { get; set; }
+
+
         public ImageViewerControlViewModel()
         {
+            Scale = 1;
+
             _shapeStyler = new ShapeStylerFactory().CustomShapeStyler(Brushes.Transparent, Brushes.Red, 5, 15);
 
             SketchBoardDataManager = new SketchBoardDataManager();
@@ -69,6 +91,17 @@ namespace Lan.Shapes.App
             GeometryTypeList = new List<GeometryType>(SketchBoardDataManager.GetRegisteredGeometryTypes()
                 .Select(x => new GeometryType(x, x, null)));
             Image = new BitmapImage(new Uri("pack://application:,,,/Lan.Shapes.App;component/reference.png"));
+
+            ZoomOutCommand = new RelayCommand(() =>
+            {
+                Scale *=(1+ _scaleIncremental);
+            });
+            ZoomInCommand = new RelayCommand(() =>
+            {
+                Scale *= (1-_scaleIncremental);
+            });
+            ScaleToFitCommand = new RelayCommand(() => Scale = -1);
+            ScaleToOriginalSizeCommand = new RelayCommand(() => Scale = 0);
         }
 
 
