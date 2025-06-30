e1190f64e8ca3f339040d70badc113ec335aca29
Wansheng , li <44959548+PawPatrolRubble@users.noreply.github.com>
2025-06-30 11:16:20 +0800

    tried to fix the bug in sketchboard

--- src/Lan.SketchBoard/SketchBoard.cs
+++ src/Lan.SketchBoard/SketchBoard.cs
@@ -211,7 +211,7 @@ namespace Lan.SketchBoard
                     else
                     {
                         //SketchBoardDataManager.SelectedGeometry = GetHitTestShape(e.GetPosition(this));
-                        SketchBoardDataManager.SelectedGeometry?.OnMouseMove(e.GetPosition(this), e.LeftButton);
+                        SketchBoardDataManager?.SelectedGeometry?.OnMouseMove(e.GetPosition(this), e.LeftButton);
                     }
                 }
             }
